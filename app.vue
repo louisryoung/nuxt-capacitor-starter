f6bca64a8646b47beedec392e60ccc531f5830f4
Louis Young <louis.young0420@gmail.com>
2023-07-13 04:30:39 +0200

    Setup

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<h1>Switzerland Time</h1>
+		<div>
+			🕥 Current time: <b>{{ timeData.datetime }}</b>
+			<a @click="$nuxt.refresh()" href="">🔄</a>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const { data: timeData } = await useFetch(
+	'https://worldtimeapi.org/api/timezone/Europe/Zurich'
+);
+</script>
